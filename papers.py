4cf56af888faeaec8dbcd7457518f3fe242fd5a7
anneventures84 <pinayanne84@gmail.com>
2014-11-09 23:41:09 -0500

    hi rana, as per our discussion, i'm adding my part. please add yours, thanks. will add comments and test tomorrow

--- papers.py
+++ papers.py
@@ -26,31 +26,7 @@ def decide(input_file, watchlist_file, countries_file):
         an entry or transit visa is required, and whether there is currently a medical advisory
     :return: List of strings. Possible values of strings are: "Accept", "Reject", "Secondary", and "Quarantine"
     """
-    with open(input_file, "r") as file_reader:
-
-        travelers_file = file_reader.read()
-        travelers_json = json.loads(travelers_file)
-
-    with open(watchlist_file, "r") as file_reader:
-        watchlist_contents = file_reader.read()
-        watchlist_json = json.loads(watchlist_contents)
-        result = []
-        
-    for passport in travelers_json:
-        traveler_passport = passport['passport']
-        found_in_watchlist = False
-        for passports in watchlist_json:
-            watchlist_passport = passports['passport']
-            if watchlist_passport == traveler_passport :
-                found_in_watchlist = True;
-        if found_in_watchlist:
-            result.append("reject")
-        else:
-            result.append("accept")
-
-    return result
-
-    #return ["Reject"]
+    return ["Reject"]
 
 
 def valid_passport_format(passport_number):
@@ -59,11 +35,10 @@ def valid_passport_format(passport_number):
     :param passport_number: alpha-numeric string
     :return: Boolean; True if the format is valid, False otherwise
     """
-    passport_format = re.compile('.{5}-.{5}-.{5}-.{5}-.{5}')
+    passport_format = re.compile('^.{5}-.{5}-.{5}-.{5}-.{5}$')
 
     if passport_format.match(passport_number):
         return True
-
     else:
         return False
 
@@ -80,5 +55,62 @@ def valid_date_format(date_string):
     except ValueError:
         return False
 
-x = decide("example_entries.json", "watchlist.json", "countries.json")
-print (x )
+
+        if traveller_status == 'Accept':
+            first_name = traveller['first_name']
+            last_name = traveller['last_name']
+            try:
+                traveller_visa_code = traveller['visa']['code']
+                traveller_visa_date = traveller['visa']['date']
+                valid_date = valid_date_format(traveller_visa_date)
+                if not valid_date:
+                    traveller_status = 'Reject'
+            except:
+                traveller_visa_code = ''
+                traveller_visa_date = ''
+
+def valid_country_list(home_country_code, from_country_code, via_country_code, traveller_entry_reason, date_today,
+                       country_list,traveller_visa_code, traveller_visa_date):
+    """
+    Checks whether the traveller needs an entry or transit visa is required, and whether there is currently a medical advisory
+    :param home_country_code: string home country of the traveller
+    :param from_country_code: string from country of the traveller
+    :param via_country_code: string via country of the traveller
+    :param traveller_entry_reason: string reason for travelling
+    :param date_today: date of processing
+    :param country_list: list of country with entry/visa requirements or has medical advisory
+    :param traveller_visa_code: string visa code/number
+    :param traveller_visa_date: date visa expiration date
+    :return: Quarantine country is in medical advisory; Reject if traveller has no entry permit or visa which is required by the destination country; Accept passed validations
+    """
+    if from_country_code != '':
+       validate_country = from_country_code
+    else:
+       if via_country_code != '':
+           validate_country = via_country_code
+       else:
+           return 'Reject'
+    try:
+        if country_list[validate_country]['medical_advisory'] != '':
+            country_traveller_status = 'Quarantine'
+        else:
+            country_traveller_status = 'Accept'
+    except:
+        country_traveller_status = 'Accept'
+    if country_traveller_status == 'Accept':
+        if home_country_code == 'KAN' and traveller_entry_reason == 'Returning':
+            country_traveller_status = 'Accept'
+        else:
+            try:
+                if (country_list[home_country_code]['visitor_visa_required'] == '1' or
+                    country_list[home_country_code]['transit_visa_required'] == '1'):
+                    if traveller_visa_code == "":
+                        country_traveller_status = 'Reject'
+                    else:
+                        if (date_today.year - traveller_visa_date.year)>= 2:
+                            country_traveller_status = 'Reject'
+                        else:
+                            country_traveller_status = 'Accept'
+            except:
+                country_traveller_status = 'Reject'
+    return country_traveller_status
